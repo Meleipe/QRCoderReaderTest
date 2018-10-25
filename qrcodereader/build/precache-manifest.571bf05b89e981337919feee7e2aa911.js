cd2090a12046f6a5b42f06d891befd4c58fec6e0
Pedro Nápoles <pedro.napoles@byside.com>
2018-10-25 16:31:10 +0100

    publish build

---
+++ qrcodereader/build/precache-manifest.571bf05b89e981337919feee7e2aa911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/QRCoderReaderTest/qrcodereader/build/index.html/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "588039d3602cef13d8fa",
+    "url": "/QRCoderReaderTest/qrcodereader/build/index.html/static/js/runtime~main.588039d3.js"
+  },
+  {
+    "revision": "2dc005b89dfb7de319b7",
+    "url": "/QRCoderReaderTest/qrcodereader/build/index.html/static/js/main.2dc005b8.chunk.js"
+  },
+  {
+    "revision": "2cc4c9f6ffd9d5451d1c",
+    "url": "/QRCoderReaderTest/qrcodereader/build/index.html/static/js/1.2cc4c9f6.chunk.js"
+  },
+  {
+    "revision": "2dc005b89dfb7de319b7",
+    "url": "/QRCoderReaderTest/qrcodereader/build/index.html/static/css/main.e83e23b9.chunk.css"
+  },
+  {
+    "revision": "d519133da07766d5e02c788a40af9c2a",
+    "url": "/QRCoderReaderTest/qrcodereader/build/index.html/index.html"
+  }
+];
