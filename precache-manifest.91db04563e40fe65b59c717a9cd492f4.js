3928c5372579e8441ee78c711cf50591bce42397
Pedro Nápoles <pedro.napoles@byside.com>
2018-10-25 16:19:39 +0100

    Updates

---
+++ precache-manifest.91db04563e40fe65b59c717a9cd492f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/byQrCode/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "241924e611978e3ad37f",
+    "url": "/byQrCode/static/js/runtime~main.241924e6.js"
+  },
+  {
+    "revision": "5da2aefd214806a2278f",
+    "url": "/byQrCode/static/js/main.5da2aefd.chunk.js"
+  },
+  {
+    "revision": "2cc4c9f6ffd9d5451d1c",
+    "url": "/byQrCode/static/js/1.2cc4c9f6.chunk.js"
+  },
+  {
+    "revision": "5da2aefd214806a2278f",
+    "url": "/byQrCode/static/css/main.e83e23b9.chunk.css"
+  },
+  {
+    "revision": "a2bdce6105ea0a9858839fdd5a543240",
+    "url": "/byQrCode/index.html"
+  }
+];
